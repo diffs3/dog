17eded8485d62fad0f0015d6b1f48add186e2f2f
diffs3 <ahges8x@outlook.com>
2024-06-06 14:15:23 +0800

    Modify food.c

--- food.c
+++ food.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Area government military peace from answer common.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
