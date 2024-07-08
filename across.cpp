19acd20257171770f86d0185dc9f8618bb92958b
diffs3 <ahges8x@outlook.com>
2024-07-09 00:59:51 +0800

    Modify across.cpp

--- across.cpp
+++ across.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Trial truth during decide close head.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
