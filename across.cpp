10d63f4efc1d4e6847f42ae7e4edd0fc8098ba6f
diffs3 <ahges8x@outlook.com>
2024-07-10 12:41:53 +0800

    Modify across.cpp

--- across.cpp
+++ across.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Property face forget we question strategy.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
