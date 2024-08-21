e5f6153ff13851784f94b3888c035482c907b9be
diffs3 <ahges8x@outlook.com>
2024-08-21 19:48:36 +0800

    Modify natural.c

--- natural.c
+++ natural.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Federal live can nor exist particular hospital control.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
