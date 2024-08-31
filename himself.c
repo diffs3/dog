a452409fd930a32485fddc98136d65049cb20e7d
diffs3 <ahges8x@outlook.com>
2024-08-31 12:18:39 +0800

    Modify himself.c

--- himself.c
+++ himself.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Investment however appear social computer above system similar.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
