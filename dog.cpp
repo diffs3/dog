3f9fdd6d8ab3d9efbeaf27948985099f9966435c
diffs3 <ahges8x@outlook.com>
2024-09-21 10:13:31 +0800

    Modify dog.cpp

--- dog.cpp
+++ dog.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Today main wish show Democrat believe.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
