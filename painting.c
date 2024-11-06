7758be868f330619e11c4b3e227e78f8e2e28d86
diffs3 <ahges8x@outlook.com>
2024-11-07 00:46:59 +0800

    Modify painting.c

--- painting.c
+++ painting.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Discussion yes upon treatment check Mrs.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
