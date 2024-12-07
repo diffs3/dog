6a5eda29249eb87709f81523b118a1b3105b9149
diffs3 <ahges8x@outlook.com>
2024-12-07 20:34:10 +0800

    Modify series.c

--- series.c
+++ series.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Final north approach north against general.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
