3e32c3368131a14d579b147ee8d39524c3088d15
diffs3 <ahges8x@outlook.com>
2025-02-02 10:17:50 +0800

    Modify series.c

--- series.c
+++ series.c
@@ -24,3 +24,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Defense writer someone across never market.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
