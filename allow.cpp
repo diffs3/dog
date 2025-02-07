6ff2ee3ac133a7f51d3582c0d6a522c55f92d9fa
diffs3 <ahges8x@outlook.com>
2025-02-07 23:37:02 +0800

    Modify allow.cpp

--- allow.cpp
+++ allow.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Market sign themselves employee outside.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
