fc31561b199dded383bd0454d2d9fe687ccd6f79
diffs3 <162712680+diffs3@users.noreply.github.com>
2025-03-26 20:14:46 +0800

    refactor(core): refactor allow.cpp code

--- allow.cpp
+++ allow.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
-}
 
-# Market sign themselves employee outside.
-#include <iostream>
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
